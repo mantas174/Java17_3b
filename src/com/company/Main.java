3041c6ba27d9ed06fd302f2ef6d518ba21695660
Mantas <mantas.muzzz@gmail.com>
2018-02-20 10:05:23 +0200

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// 3b.
+        //​
+        // Parašyti programą kuri paprašytų vartotojo įvesti  5 žodžius.  Po to kai bus įvesti visi
+        //žodžiai, jie turi būti atvaizduojami
+
+
+    }
+}
